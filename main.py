fb27f4960fd1301667388e6b67e610efa6fc86dc
Konscription <konviction@gmail.com>
2024-09-05 14:32:59 -0400

    added sprite groups for object handeling

--- main.py
+++ main.py
@@ -6,18 +6,29 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))   
     clock = pygame.time.Clock()
+    
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
-    
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill("black")
-        player.update(dt)
-        player.draw(screen)       
+            
+        for obj in updatable:
+            obj.update(dt)
+              
+        screen.fill("black")       
+
+        for obj in drawable:
+            obj.draw(screen)
+  
         pygame.display.flip()
         
         # limit framerate to specified FPS
