70061437b73d667b3f96bc56dc3e935da41ccf6d
Konscription <konviction@gmail.com>
2024-09-05 14:10:01 -0400

    implement player

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))   
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
     
     
@@ -14,8 +15,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.draw(screen)       
         pygame.display.flip()
+        
+        # limit framerate to specified FPS
         dt = clock.tick(FPS) /1000
 
 if __name__ == "__main__":
