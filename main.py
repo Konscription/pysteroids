e23af65d682e2c9c0ad14aaf3c81c9cc941936d0
Konscription <konviction@gmail.com>
2024-09-05 11:55:58 -0400

    refactored FPS to a constant

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     
-    fps = 60
     clock = pygame.time.Clock()
     dt = 0
     
@@ -17,7 +16,7 @@ def main():
         
         screen.fill((0,0,0))
         pygame.display.flip()
-        dt = clock.tick(fps) /1000
+        dt = clock.tick(FPS) /1000
 
 if __name__ == "__main__":
     main()
