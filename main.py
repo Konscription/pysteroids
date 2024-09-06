35f039272578d529fe5b859be0d166eb0ee9809d
Konscription <konviction@gmail.com>
2024-09-06 15:08:01 -0400

    added asteroid splitting and distruction

--- main.py
+++ main.py
@@ -38,7 +38,13 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
             
+
+                
         screen.fill("black")       
 
         for obj in drawable:
